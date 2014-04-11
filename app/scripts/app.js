e9955de8c407fe8e80d35c009076eff330c6326c
Eric Feng <chienyuan.feng@gmail.com>
2014-04-11 17:06:34 -0500

    install by yo

---
+++ app/scripts/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular
+  .module('chienApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'MainCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
