92f424f8312c8b94c1a5d84320cba56f1e681e25
Eric Feng <chienyuan.feng@gmail.com>
2014-09-03 13:24:03 -0500

    add two more images and try to fix glyphicons issue

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,5 +10,7 @@ angular.module('chienApp')
     $scope.slides = [
       {'image':"images/c1.jpg",'text': 'Allen,TX firework'},
       {'image':"images/c2.jpg",'text': 'Allen,TX firework'},
+      {'image':"images/c3.jpg",'text': 'Allen,TX firework'},
+      {'image':"images/c4.jpg",'text': 'Allen,TX firework'},
     ];
   });
